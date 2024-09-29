b7ffe14fd8937762e21d906d1740ab83ff23f2df
Alessio Giannoccaro <mrbigalessio@gmail.com>
2024-09-30 00:04:01 +0200

    salva del 30-09-2024 ore 00.03

---
+++ 330-vini/src/components/Tabs.vue
@@ -0,0 +1,86 @@
+<script setup>
+
+import { ref } from 'vue'
+
+let tab = ref(0);
+
+
+
+</script>
+
+
+
+<!--Tabs per la navigazione-->
+
+<template>
+
+  <div class="w-full">
+
+    <div class="flex justify-between p-5">
+
+      <div 
+      
+      @click="tab=0"
+
+      >
+
+        <img src="../media/Petruzzelli.jpg" alt="logo" width="100">
+
+      </div>
+
+      
+      <button
+
+      @click="tab=1"
+
+      :class="{'btn-neutral' : tab === 1}"
+      
+      class="btn btn-xs sm:btn+-sm md:btn-md lg:btn-lg ">
+
+        HOME
+
+      </button>
+
+      <button
+
+      @click="tab=2"
+
+      :class="{'btn-neutral' : tab === 2}"
+      
+      class="btn btn-xs sm:btn+-sm md:btn-md lg:btn-lg ">
+
+        PRODOTTI
+
+      </button>
+
+
+    </div>
+
+    <!--Contenuto-->
+
+    <div>
+
+      <div v-if="tab === 0" class="py-10 px-6">
+        <img src="../media/Petruzzelli.jpg" alt="">
+      </div>
+
+      <div v-if="tab === 1" class="py-10 px-6">
+        <img src="../media/Petruzzelli.jpg" alt="">
+      </div>
+
+      <div v-if="tab === 2"  class="py-10 px-6">
+        <h1>contenuto 2</h1>
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+  </div>
+
+
+</template>
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # 330-vini
 inizio progetto il 08-09-2024 strumenti usati Vue.js
-# TEST 
-prova 1
+
+# ORDINARE IL FILE COMBINAZIONE
+
+Shift+option+F
+
+# SCORCIATOIA CHATGBT SU MAC
+
+option+space
+
+# PARENTESI GRAFFE SU MAC
+
+Shift+option+è
+
+## INSTALAZIONE
+
+
+
